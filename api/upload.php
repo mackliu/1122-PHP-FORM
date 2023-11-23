16ee9d18c26c34c0c75a1f4894be3d37ddfde9dd
mackliu <macklun@gmail.com>
2023-11-23 09:06:31 +0800

    建立檔案管理資料表

--- api/upload.php
+++ api/upload.php
@@ -1,15 +1,15 @@
 <?php
 
-echo $_POST['name'];
-echo "<br>";
+/* echo $_POST['name'];
+echo "<br>"; */
 if(!empty($_FILES['img']['tmp_name'])){
-    echo $_FILES['img']['tmp_name'];
+/*     echo $_FILES['img']['tmp_name'];
     echo "<br>";
     echo $_FILES['img']['name'];
     echo "<br>";
     echo $_FILES['img']['type'];
     echo "<br>";
-    echo $_FILES['img']['size'];
+    echo $_FILES['img']['size']; */
     $tmp=explode(".",$_FILES['img']['name']);
     $subname=".".end($tmp);
     $filename=date("YmdHis").rand(10000,99999).$subname;
