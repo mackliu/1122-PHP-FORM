4338a1c821d006aaa9dd28ba023d0719b7223532
mackliu <macklun@gmail.com>
2023-11-23 09:51:52 +0800

    引入bootstrap美化畫面

--- manage.php
+++ manage.php
@@ -16,6 +16,7 @@ include_once "db.php";
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>檔案管理功能</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
 <body>
 <h1 class="header">檔案管理練習</h1>
@@ -26,27 +27,39 @@ include_once "db.php";
 
 
 <!----透過資料表來顯示檔案的資訊，並可對檔案執行更新或刪除的工作----->
+<?php
+$files=all('files');
 
-<table>
-    <tr>
-        <td>id</td>
-        <td>檔名</td>
-        <td>類型</td>
-        <td>大小</td>
-        <td>描述</td>
-        <td>上傳時間</td>
-    </tr>
-    <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-    </tr>
-</table>
+?>
+<div class="col-8 mx-auto">
+    <table class='table table-success'>
+        <tr>
+            <td>id</td>
+            <td>檔名</td>
+            <td>類型</td>
+            <td>大小</td>
+            <td>描述</td>
+            <td>上傳時間</td>
+        </tr>
+    <?php
+    foreach($files as $file){
+    ?>
+        <tr>
+            <td><?=$file['id'];?></td>
+            <td><?=$file['name'];?></td>
+            <td><?=$file['type'];?></td>
+            <td><?=$file['size'];?></td>
+            <td><?=$file['desc'];?></td>
+            <td><?=$file['create_at'];?></td>
+        </tr>
+    <?php
+    }
+    ?>    
+    </table>
+</div>
 
 
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
